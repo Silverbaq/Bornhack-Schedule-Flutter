3f6658af8b924bf3c4e632fe8ac909c161983f3d
silverbaq <feber.sm@gmail.com>
2026-07-14 21:36:37 +0200

    feat: add ScheduleStorage and expose raw XML from ScheduleApi

---
+++ lib/utils/schedule_storage.dart
@@ -0,0 +1,17 @@
+import 'package:injectable/injectable.dart';
+import 'package:localstorage/localstorage.dart';
+
+@singleton
+class ScheduleStorage {
+  final LocalStorage _storage = LocalStorage('schedule');
+
+  Future<String?> getXml() async {
+    await _storage.ready;
+    return _storage.getItem('xml') as String?;
+  }
+
+  Future<void> saveXml(String xml) async {
+    await _storage.ready;
+    _storage.setItem('xml', xml);
+  }
+}
